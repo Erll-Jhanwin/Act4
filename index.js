bef9f21062e8998e3f4a840d3e9e1d2b1418b0d1
Erll-Jhanwin <erlljhanwincatapang61@gmail.com>
2025-09-16 21:42:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function () {
+    // Toggle the open class
+    dropDownMenu.classList.toggle('open');
+    
+    // Check if dropdown is open
+    const isOpen = dropDownMenu.classList.contains('open');
+    
+    // Change icon based on dropdown state
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+};
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
